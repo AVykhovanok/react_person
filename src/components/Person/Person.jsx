0ed5018bd7f1fe7d591828270904926eef19a097
Oles <a.vykhovanok@gmail.com>
2025-02-22 00:11:15 +0200

    v4.2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,17 +5,18 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
       {person.age != null && <p className="Person__age">I am {person.age}</p>}
-
       {() => {
         if (!person.isMarried) {
-          <p className="Person__partner">I am not married</p>;
+          return <p className="Person__partner">I am not married</p>;
         }
 
         if (person.sex === 'm') {
-          <p className="Person__partner">{person.partnerName} is my wife</p>;
-        } else {
-          <p className="Person__partner">{person.partnerName} is my husband</p>;
+          return (
+            <p className="Person__partner">{person.partnerName} is my wife</p>
+          );
         }
+
+        return (<p className="Person__partner">{person.partnerName} is my husband</p>);
       }}
     </section>
   );
